5c118be4666137ef2aee4c7252e447f9605cfb54
gabrieloli21 <gabrieloliveira62@yahoo.com>
2023-02-09 15:24:30 -0300

    Refatoração para reconhecer o FTP

--- src/views/nivo/VisualizaFtp/CardifFtp.js
+++ src/views/nivo/VisualizaFtp/CardifFtp.js
@@ -1,15 +1,18 @@
+require('dotenv').config({path: '../../../../.env'})
+
 let Client = require('ssh2-sftp-client');
 let sftp = new Client();
 
 sftp.connect({
-  host: HOST_CARDIF,
-  port: PORT_CARDIF,
-  username: USER_CARDIF,
-  password: PASSWORD_CARDIF
+  
+  host:process.env.HOST_CARDIF,
+  port:process.env.PORT_CARDIF,
+  username: process.env.USER_CARDIF,
+  password: process.env.PASSWORD_CARDIF
 }).then(() => {
   return sftp.list('/O0055CARDIF/ENTRADA');
-}).then(data => {
-  console.log(data, 'the data info');
+}).then(dados => {
+  console.log(dados, 'the data info');
 }).catch(err => {
   console.log(err, 'catch error');
 });
