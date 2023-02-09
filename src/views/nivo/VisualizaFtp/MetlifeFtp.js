6eeffb11952aa159d3fd27d3e5cb4705e4c1e6b4
gabrieloli21 <gabrieloliveira62@yahoo.com>
2023-02-09 17:21:59 -0300

    Conexão com FTPs

--- src/views/nivo/VisualizaFtp/MetlifeFtp.js
+++ src/views/nivo/VisualizaFtp/MetlifeFtp.js
@@ -1,21 +1,18 @@
 require('dotenv').config({ path: '../../../../.env' })
 
+
 let Client = require('ssh2-sftp-client');
 let sftp = new Client();
+const fs = require('fs');
 
 sftp.connect({
-  host: process.env.HOST_CARDIF,
-  port: process.env.PORT_CARDIF,
-  username: process.env.USER_CARDIF,
-  password: process.env.PASSWORD_CARDIF
+  host: process.env.HOST_METLIFE,
+  username: process.env.USER_METLIFE,
+  privateKey: fs.readFileSync(process.env.KEY_METLIFE)
 }).then(() => {
-  return sftp.list('/O0055CARDIF/ENTRADA');
+  return sftp.list('/Remessa');
 }).then(dados => {
   console.log(dados, 'Retorno dos dados');
 }).catch(err => {
   console.log(err, 'Erro');
 });
-
-const CardifFiles = () => {
-
-}
